10d9136004888f0574b08e95e177ca1d661325a7
DMario1014 <ak26mario@gmail.com>
2025-10-07 21:18:36 +0200

    asd

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//asd
+
+const typed = new Typed('.multiple', {
+    strings: ['2D Animator', 'Character Designer', 'Video Editor'],
+    typeSpeed: 100,      // Gépelési sebesség
+    backSpeed: 100,      // Törlési sebesség
+    backDelay: 1000,     // Mennyi idő után törli
+    loop: true           // Végtelen ciklus
+});
